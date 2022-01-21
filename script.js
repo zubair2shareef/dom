8eba36eb92381f380968199aef372b7799e1ede1
zubair1shareef <66367807+zubair1shareef@users.noreply.github.com>
2022-01-21 22:52:55 +0530

    queryselectorall

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-// console.log(document.all)
-// console.log(document.title.innerText)
-// document.title="hello"
-// document.querySelector("header-title").style.borderBottom="solid 3px #000git init"
+// // console.log(document.all)
+// // console.log(document.title.innerText)
+// // document.title="hello"
+// // document.querySelector("header-title").style.borderBottom="solid 3px #000git init"
 
-document.getElementsByClassName('items-list')[1].innerText="zubair"
+// document.getElementsByClassName('items-list')[1].innerText="zubair"
 
-document.getElementsByClassName('items-list')[2].style.backgroundColor="green"
-
-
-var items=document.getElementsByClassName('items-list');
+// document.getElementsByClassName('items-list')[2].style.backgroundColor="green"
 
 
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold'
-}
-console.log(document.getElementsByClassName('items-list'))
+// var items=document.getElementsByClassName('items-list');
+
+
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold'
+// }
+// console.log(document.getElementsByClassName('items-list'))
 
 
 
 
-var item=document.getElementsByTagName('li');
+// var item=document.getElementsByTagName('li');
 
 
-for(let i=0;i<items.length;i++){
-    item[i].style.backgroundColor='red'
-}
-console.log(document.getElementsByClassName('items-list'))
+// for(let i=0;i<items.length;i++){
+//     item[i].style.backgroundColor='red'
+// }
+// console.log(document.getElementsByClassName('items-list'))
 
-
+// //-------------queryselector------------
 
 var item2=document.querySelector('.items-list:nth-child(2)');
-item2.style.backgroundColor="green"
+item2.style.backgroundColor="blue"
 
 
 var item3=document.querySelector('.items-list:nth-child(3)');
-item3.style.display="none"
+item3.style.co
 
+//-------------queryselectorall------------
+
+var itemm=document.querySelectorAll(".items-list");
+itemm[3].style.color="green"
+
+var odd=document.querySelectorAll("li:nth-child(odd)");
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor="green"
+}
